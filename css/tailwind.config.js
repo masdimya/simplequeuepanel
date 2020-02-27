6f7421b94e289d32ee25224bc1896900e65f3904
masdimya <dimas6597@gmail.com>
2020-02-27 21:02:47 +0700

    first commit

---
+++ css/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    borderRadius: {
+      'none'  : '0',
+      'sm'    : '0.125rem',
+       default: '0.25rem',
+      'md'    : '0.375rem',
+      'lg'    : '0.5rem',
+      'full'  : '9999px',
+      'large' : '1.5rem',
+    },
+    fontSize: {
+      'xs'  : '.75rem',
+      'sm'  : '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg'  : '1.125rem',
+      'xl'  : '1.25rem',
+      '2xl' : '1.5rem',
+      '3xl' : '1.875rem',
+      '4xl' : '2.25rem',
+      '5xl' : '3rem',
+      '6xl' : '4rem',
+      '7xl' : '5rem',
+      'xxl' : '11rem',
+
+    }
+  },
+  variants: {},
+  plugins: [],
+}
